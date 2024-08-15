eb0a653beffead06e22249df81324c46d196e299
lytysov Artem <a.lytysov@gmail.com>
2024-08-15 13:20:51 +0300

    first commit

---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "folder_id" {
+  description = "Default folder ID in yandex cloud"
+  type        = string
+  default     = "b1gt935u4n3dtlivmp2h"
+}
+variable "cloud_id" {
+  type =  string
+  default = "b1gh7ap8062c6e5vu7gm"
+}
